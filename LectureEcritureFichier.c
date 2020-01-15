9aedfdec314dddddc88ff2a13dcc11d95e11fcd5
Benoit Bouckaert <benoit.bouckaert@mci.fr>
2020-01-15 11:36:46 +0100

    Creation fonction Lecture fichier pour test

--- LectureEcritureFichier.c
+++ LectureEcritureFichier.c
@@ -8,7 +8,7 @@
  * 0 : fichier écrit correctement
  * 1 : Erreut de création ou d'ouverture du fichier
  */
-int EcrireLabyrinthe(char lab[TAILLE_MAX][TAILLE_MAX], const char fichier) {
+int EcrireLabyrinthe(char lab[TAILLE_MAX][TAILLE_MAX], const char *fichier){
     FILE *StFichier = NULL;
 
     StFichier = fopen(fichier, "w+");
@@ -16,7 +16,7 @@ int EcrireLabyrinthe(char lab[TAILLE_MAX][TAILLE_MAX], const char fichier) {
     if (StFichier != NULL){
         for (unsigned int j = 0 ; j<TAILLE_MAX ; j++){
             for (unsigned int i = 0 ; i<TAILLE_MAX ; i++){
-                fprintf(fichier, "%i,", lab[i][j]);
+                fprintf(StFichier, "%i,", lab[i][j]);
                 if (lab[i][j] == 5){
                     i = TAILLE_MAX;
                 }
@@ -27,11 +27,38 @@ int EcrireLabyrinthe(char lab[TAILLE_MAX][TAILLE_MAX], const char fichier) {
             }
         }
 
-        fclose(fichier);
+        fclose(StFichier);
     }
     else {
         return 1;
     }
 
     return 0;    
+}
+
+int LireLabyrinthe(char lab[TAILLE_MAX][TAILLE_MAX], const char *fichier){
+    FILE *StFichier = NULL;
+
+    StFichier = fopen(fichier, "r");
+
+    if(StFichier != NULL){
+        for (unsigned int j = 0 ; j<TAILLE_MAX ; j++){
+            for (unsigned int i = 0 ; i<TAILLE_MAX ; i++){
+                lab[i][j] = fgetc(StFichier);
+                if (lab[i][j] == 5){
+                    i = TAILLE_MAX;
+                }
+                if (lab[i][j] == 6){
+                    i = TAILLE_MAX;
+                    j = TAILLE_MAX;
+                }
+            }
+        }
+        fclose(StFichier);
+    }
+    else{
+        return 1;
+    }
+    
+    return 0;
 }
