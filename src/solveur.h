8f010037957f25412cfb5e840da4dea600f1e52d
ThomasObled <thomas.obledto@gmail.com>
2020-01-29 11:33:08 +0100

    Solveur

--- src/solveur.h
+++ src/solveur.h
@@ -1,5 +1,12 @@
 #include "constants.h"
 
+/*  ~ DernierMvt ~
+     *   0 : Droite
+     *   1 : Bas
+     *   2 : Gauche
+     *   3 : Haut
+*/
+
 //Renvoient la Hauteur et la Longeur du labyrinthe.
 int TrouverLongeur(char tab[TAILLE_MAX][TAILLE_MAX]);
 int TrouverHauteur(char tab[TAILLE_MAX][TAILLE_MAX], int Longeur);
@@ -10,10 +17,8 @@ int TrouverDepart(char tab[TAILLE_MAX][TAILLE_MAX],int Longeur);
 //Renvoie le nombre de touche directes (droite,gauche,haut,bas) de la case concernée.
 int NombreToucheR(char tab[TAILLE_MAX][TAILLE_MAX],int *i, int *j);
 
-//Sort le solveur d'une impasse.
+//Sortent le solveur d'une impasse.
 void Rembobine(char tab[TAILLE_MAX][TAILLE_MAX],int *i, int *j, int *dernierMvt);
-
-//
 int TrouveRetour(char gauche, char droite, char haut, char bas);
 
 //Déplacent le solveur dans le labyrinthe.
@@ -22,7 +27,7 @@ void Descend(char tab[TAILLE_MAX][TAILLE_MAX], int *i, int *j, int *dernierMvt);
 void Gauche(char tab[TAILLE_MAX][TAILLE_MAX], int *i, int *j, int *dernierMvt);
 void Monte(char tab[TAILLE_MAX][TAILLE_MAX], int *i, int *j, int *dernierMvt);
 
-//
+//Trouve la prochaine direction à prendre pour le solveur en fonction du dernier mouvement et des cases qui l'entourent.
 void Deplacement(char tab[TAILLE_MAX][TAILLE_MAX],int *i, int *j, int *dernierMvt);
 
 //Routine complète de résolution d'un labyrinthe.
